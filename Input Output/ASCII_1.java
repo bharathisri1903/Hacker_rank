e7c778548609de1be5e6cae889be6379e1ec37b9
BHARATHI SRI K V <117757826+bharathisri1903@users.noreply.github.com>
2024-03-26 16:15:57 +0530

    ASCII-1

---
+++ Input Output/ASCII_1.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class ASCII_1{
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        char ch=sc.next().charAt(0);
+        int a=ch;
+        System.out.println(a);
+    }
+}

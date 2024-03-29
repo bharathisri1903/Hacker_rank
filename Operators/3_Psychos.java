b71136ae1130a42d12f37192f0414825022a4e13
BHARATHI SRI K V <117757826+bharathisri1903@users.noreply.github.com>
2024-03-29 19:37:48 +0530

    Add files via upload

---
+++ Operators/3_Psychos.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class 3_Psychos{
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int x1=sc.nextInt();
+        int y1=sc.nextInt();
+        int x2=sc.nextInt();
+        int y2=sc.nextInt();
+        double x=(x1+x2)/2.0;
+        double y=(y1+y2)/2.0;
+        System.out.println("Arun's house is located at("+(float)x+ ","+(float)y+")");
+        
+    }
+}
